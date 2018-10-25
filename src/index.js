2a5b475ececa84231719f404b49fea3394a73abb
Sergey Hovakimyan <s.hovakimyan@apollobytes.com>
2018-10-25 23:24:51 +0400

    Add minimial needed features

--- src/index.js
+++ src/index.js
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Index from './containers/root';
 import * as serviceWorker from './serviceWorker';
+import { HashRouter } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+
+const theme = createMuiTheme({
+    palette: {
+        primary: blue,
+        secondary: blue,
+    },
+});
+
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+        <HashRouter>
+            <Index />
+        </HashRouter>
+    </MuiThemeProvider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
